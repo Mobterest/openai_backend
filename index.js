1c32a5be742f9107356acad1b1064067ae52a362
Mobterest Studio <mobterest@gmail.com>
2023-02-01 04:21:16 +0300

    Update API Key

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json({extended: true}));
 
-const port = 1000;
+const port = 4000;
 
 const {Configuration, OpenAIApi} = require('openai');
 const configuration = new Configuration({
-    apiKey: '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'
+    apiKey: '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'
 })
 const openai = new OpenAIApi(configuration);
 
 
 app.post('/request', async function(req, res) {
+    console.log(req.body.widget +  "(//Dart \n //" +req.body.prompt + " \n");
     const response = await openai.createCompletion({
         model: "code-davinci-002",
         prompt: req.body.widget +  "(//Dart \n //" +req.body.prompt + " \n",
@@ -21,6 +22,7 @@ app.post('/request', async function(req, res) {
         temperature: 0,
         stop: "\n"
     });
+    console.log(response.data.choices);
 
     res.send({data: response.data.choices[0].text})
 })
